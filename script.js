a11bc19c7063e9537bde6c1604cb8677e6719479
orionserrano115 <130587502+orionserrano115@users.noreply.github.com>
2026-04-29 18:46:45 -0500

    cambio 3
    se separan los archivos

---
+++ script.js
@@ -0,0 +1,21 @@
+// script.js
+document.getElementById("formularioUsuario").addEventListener("submit", function(e) {
+    let nombre = document.querySelector("input[name='nombre']").value.trim();
+    let cedula = document.querySelector("input[name='cedula']").value.trim();
+    let telefono = document.querySelector("input[name='telefono']").value.trim();
+
+    if (nombre === "" || cedula === "" || telefono === "") {
+        alert("Todos los campos son obligatorios");
+        e.preventDefault();
+    }
+
+    if (cedula.length < 5) {
+        alert("La cédula debe tener al menos 5 caracteres");
+        e.preventDefault();
+    }
+
+    if (telefono.length < 7) {
+        alert("El teléfono debe tener al menos 7 caracteres");
+        e.preventDefault();
+    }
+});
